fa7722f6cfdc07bce494f2bf60a278e1aa22ca28
tariqul <tariqul1993#gmail.com>
2022-01-08 01:05:33 +0600

    first commit

---
+++ ImagePickerGalleryCameraSwiftUI/ImagePickerView.swift
@@ -0,0 +1,20 @@
+//
+//  ImagePickerView.swift
+//  ImagePickerGalleryCameraSwiftUI
+//
+//  Created by Tariqul on 7/1/22.
+//
+
+import SwiftUI
+
+struct ImagePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImagePickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagePickerView()
+    }
+}
