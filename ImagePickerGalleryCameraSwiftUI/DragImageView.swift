0b7836a5e78573bc5dee815fff110ede2cb5a37a
simec mac-01 <tariqul1993@gmail.com>
2022-01-09 17:58:59 +0600

    image picker with design

--- ImagePickerGalleryCameraSwiftUI/DragImageView.swift
+++ ImagePickerGalleryCameraSwiftUI/DragImageView.swift
@@ -15,7 +15,7 @@ struct DragImageView: View {
     @State private var currentWidth: CGFloat = 100
     @State private var addedImages = [UIImage]()
     @State private var isShowPhotoLibrary = false
-
+    
     var bindingForImage: Binding<UIImage> {
         Binding<UIImage> { () -> UIImage in
             return addedImages.last ?? UIImage()
@@ -24,46 +24,62 @@ struct DragImageView: View {
             print("Images: \(addedImages.count)")
         }
     }
-    
+    let columns = [
+        GridItem(.fixed(200)),
+        GridItem(.fixed(200)),
+    ]
     var body: some View {
-        VStack(spacing: 12) {
-            imageView
-        }
         
-        // Gets a new Image
-        Button {
-            self.isShowPhotoLibrary = true
-        } label: {
-            Text("Add Image")
-                .foregroundColor(Color.yellow)
-        }.sheet(isPresented: $isShowPhotoLibrary) {
-            ImagePicker(sourceType: .photoLibrary, selectedImage: bindingForImage)
-        }
-        Spacer()
+        VStack{
+            VStack() {
+                imageView
+            }
+            
+            // Gets a new Image
+            Button {
+                self.isShowPhotoLibrary = true
+            } label: {
+                Text("Add Image")
+                    .frame(width: UIScreen.main.bounds.size.width/2-10, height: UIScreen.main.bounds.size.width/2-10)
+                                .font(.system(size: 18))
+                                .padding()
+                                .foregroundColor(.white)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 25)
+                                        .stroke(Color.gray, lineWidth: 2))
+            }.sheet(isPresented: $isShowPhotoLibrary) {
+                ImagePicker(sourceType: .photoLibrary, selectedImage: bindingForImage)
+            }
+            Spacer()
+        } .frame(width: UIScreen.main.bounds.size.width-100)
     }
+    
+    
     
     var imageView: some View {
         VStack() {
-            ForEach(addedImages, id: \.self) { image in
-                //DraggableImage(image: image)
-                ZStack(alignment: .top){
-                    Image(uiImage: image)
-                        .resizable()
-                        .scaledToFill()
-                    HStack{
-                        Spacer()
-                        Image("remove")
+            LazyVGrid(columns: columns, spacing: 2) {
+                ForEach(addedImages, id: \.self) { image in
+                    ZStack(alignment: .top){
+                        Image(uiImage: image)
                             .resizable()
-                            .scaledToFit()
-                            .frame(width: 30, height: 30)
-                            .onTapGesture {
-                                addedImages.remove(at: 0)
-                            }
-                    }.frame(maxWidth: .infinity, alignment: .trailing)
+                            .scaledToFill()
+                            .frame(width: (UIScreen.main.bounds.size.width/2)-50)
+                        HStack{
+                            Spacer()
+                            Image("remove")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 30, height: 30)
+                                .onTapGesture {
+                                    addedImages.remove(at: 0)
+                                }
+                        }.frame(maxWidth: (UIScreen.main.bounds.size.width/2)-50, alignment: .trailing)
+                    } .frame(width: (UIScreen.main.bounds.size.width/2)-60)
                     
-                   
-                }.frame(width: 100, height: 100, alignment: .topTrailing)
+                }
             }
+            
         }
     }
     
